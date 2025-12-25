643e3d5c9b26fdc83f9d78654b5736e5eed0cb83
RahulShirke28804 <rahulshirke28804@gmail.com>
2025-12-25 23:58:20 +0530

    Update delete.c

--- Assignment_3/delete.c
+++ Assignment_3/delete.c
@@ -4,24 +4,32 @@
 • Delete files with size = 0 bytes (regular files only).
 • Print names deleted + total deleted count.
 */
-
-#include<stdio.h>
-#include<dirent.h>
-#include<string.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 int main()
 {
-    char dirname[20];
+    char dirname[256];
     DIR *dp;
     struct dirent *ptr;
-
-    memset(dirname, '\0', sizeof(dirname));
+    struct stat sobj;
+    int count = 0;
 
     printf("Enter the name of the directory : ");
     scanf("%s", dirname);
 
-    dp = opendir(dirname);
+    // change working directory
+    if(chdir(dirname) == -1)
+    {
+        perror("Unable to change directory");
+        return -1;
+    }
 
+    // open current directory now
+    dp = opendir(".");
     if(dp == NULL)
     {
         perror("Unable to open directory");
@@ -30,14 +38,25 @@ int main()
 
     while((ptr = readdir(dp)) != NULL)
     {
-        if((ptr->d_reclen) == 0)
-        {
-            unlink(ptr->d_name);
-        }
+        if(strcmp(ptr->d_name, ".") == 0 || strcmp(ptr->d_name, "..") == 0)
+            continue;
 
+        if(lstat(ptr->d_name, &sobj) == -1)
+            continue;
+
+        if(S_ISREG(sobj.st_mode) && sobj.st_size == 0)
+        {
+            if(unlink(ptr->d_name) == 0)
+            {
+                printf("Deleted: %s\n", ptr->d_name);
+                count++;
+            }
+        }
     }
 
     closedir(dp);
+
+    printf("\nTotal files deleted: %d\n", count);
 
     return 0;
 }
